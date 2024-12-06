b094cbe8e5f3440cabf49caa86a64f6f41456525
Jonathan Wyse <wyseguyonline@gmail.com>
2024-12-06 11:48:21 -0700

    remove unnecessary function from input component

--- src/movieContainer/SearchInput.js
+++ src/movieContainer/SearchInput.js
@@ -3,16 +3,12 @@ import {useRef} from 'react';
 const SearchInput = ({searchStrHandler}) => {
   const searchStr = useRef('');
 
-  const searchHandler = () => {
-    searchStrHandler(searchStr.current);
-  };
-
   return (
     <>
       <h1>Movie Search!</h1>
       <div>
         <input type="text" onChange={(e) => searchStr.current = e.target.value} />
-        <button onClick={searchHandler}>Search</button>
+        <button onClick={() => searchStrHandler(searchStr.current)}>Search</button>
       </div>
     </>
   );
